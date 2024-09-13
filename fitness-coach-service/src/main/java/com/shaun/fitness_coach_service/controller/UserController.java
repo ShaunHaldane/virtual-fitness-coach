bdeed9dde8419c6746e9480771f764a849138b57
Shaun-Haldane <shaun26haldane@gmail.com>
2024-09-13 09:52:50 +0200

    [add] Validations for user. Changed createUser in controller from private to public to solve nullPointerException on userService

--- fitness-coach-service/src/main/java/com/shaun/fitness_coach_service/controller/UserController.java
+++ fitness-coach-service/src/main/java/com/shaun/fitness_coach_service/controller/UserController.java
@@ -4,9 +4,10 @@ import com.shaun.fitness_coach_service.entity.User;
 import com.shaun.fitness_coach_service.entity.Video;
 import com.shaun.fitness_coach_service.service.UserService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -14,19 +15,20 @@ import java.util.Set;
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/user")
-@RequiredArgsConstructor
+@AllArgsConstructor
+@Validated
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping()
-    private ResponseEntity<User> createUser(@Valid @RequestBody User user){
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
         User savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    private ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user){
+    public ResponseEntity<User> updateUser(@Valid @PathVariable("id") long id, @RequestBody User user){
         User updatedUser = userService.updateUser(id, user);
         return new ResponseEntity<>(updatedUser, HttpStatus.CREATED);
     }
--- fitness-coach-service/src/main/java/com/shaun/fitness_coach_service/entity/User.java
+++ fitness-coach-service/src/main/java/com/shaun/fitness_coach_service/entity/User.java
@@ -7,6 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -15,10 +19,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "Name can not be a null or empty")
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
     private String name;
+    @NotEmpty(message = "Email address can not be a null or empty")
+    @Email(message = "Email address should be a valid value")
     private String email;
 
     // Payment details
+    @NotEmpty(message = "Account number can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "Account number must be 10 digits")
     private String cardNumber;
 
     // User
